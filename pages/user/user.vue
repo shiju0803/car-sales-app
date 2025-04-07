7cb31ffb79ecff3dcf9af375cd3b4f11e48a5de7
shiju <shiju1283@163.com>
2025-04-07 14:51:57 +0800

    refactor(user): 移除我的评价功能
    - 删除了用户页面中的"我的评价"列表单元- 优化了用户页面的界面布局

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -25,7 +25,6 @@
 				<list-cell icon="icon-lishijilu" iconColor="#e07472" title="我的足迹" @eventClick="navTo('/pages/user/readHistory')"></list-cell>
 				<list-cell icon="icon-shoucang" iconColor="#5fcda2" title="我的关注" @eventClick="navTo('/pages/user/brandAttention')"></list-cell>
 				<list-cell icon="icon-shoucang_xuanzhongzhuangtai" iconColor="#54b4ef" title="我的收藏" @eventClick="navTo('/pages/user/productCollection')"></list-cell>
-				<list-cell icon="icon-pingjia" iconColor="#ee883b" title="我的评价"></list-cell>
 				<list-cell icon="icon-shezhi1" iconColor="#e07472" title="设置" border="" @eventClick="navTo('/pages/set/set')"></list-cell>
 			</view>
 		</view>
